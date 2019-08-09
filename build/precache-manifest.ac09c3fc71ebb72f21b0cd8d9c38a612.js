7a8928dc55048357ae2601bf2eeb63cd9b78cfdf
John Espiritu Sebulino <John E Sebulino@fp.manulife.hk>
2019-08-10 02:00:53 +0800

    UPloading build

---
+++ build/precache-manifest.ac09c3fc71ebb72f21b0cd8d9c38a612.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d3b3ba80fb4a2ef848f28fd37b0dccd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a9b25ee337a330080b8",
+    "url": "/static/css/main.b33eb5c9.chunk.css"
+  },
+  {
+    "revision": "14b3209e527a5cb7f2e2",
+    "url": "/static/js/2.d9456afe.chunk.js"
+  },
+  {
+    "revision": "6a9b25ee337a330080b8",
+    "url": "/static/js/main.dbc778e5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3045c4d7454b74ad3f9113d4159d1e9a",
+    "url": "/static/media/art1.3045c4d7.JPG"
+  },
+  {
+    "revision": "31e2c721c289c59f05515ecba272b52d",
+    "url": "/static/media/art2.31e2c721.JPG"
+  },
+  {
+    "revision": "4453b1282b93d57b8f9edceb71a5df1b",
+    "url": "/static/media/crizell_portfolio.4453b128.pdf"
+  },
+  {
+    "revision": "af6b8e7801632bc144dd2f63b4c0f0e4",
+    "url": "/static/media/crizell_resume.af6b8e78.pdf"
+  },
+  {
+    "revision": "03e747680ef78352fdfedfb90caac148",
+    "url": "/static/media/kitel.03e74768.jpg"
+  }
+]);
